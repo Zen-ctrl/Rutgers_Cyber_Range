56027c7cae2a9aa9d043be5ecc1f0111170351a0
kwxk <76072619+kwxk@users.noreply.github.com>
2022-12-19 12:03:19 -0500

    Update kwxk-ansible-installer.sh

--- kwxk-ansible-installer.sh
+++ kwxk-ansible-installer.sh
@@ -1,67 +1,57 @@
 #!/bin/bash
 
-# Ask the user for their operating system
-echo "What is your operating system? (linux, windows, or MACOS)"
-read os
+echo "Which operating system would you like to use?"
+echo "1. macOS"
+echo "2. Linux"
+echo "3. Windows"
 
-# Install Chocolatey on macOS
-if [ "$os" == "windows" ]; then
-  # Install Chocolatey on Windows
-  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-elif [ "$os" == "MACOS" ]; then
-  # Install Chocolatey on macOS (using Homebrew)
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  brew install chocolatey
-fi
+read -p "Enter your choice (1, 2, or 3): " choice
 
-# Install Homebrew on macOS
-if [ "$os" == "MACOS" ]; then
-  # Check if Homebrew is already installed
-  if ! [ -x "$(command -v brew)" ]; then
-    # Install Homebrew
+if [ "$choice" = "1" ] || [ "$choice" = "macOS" ]; then
+  # Install brew and ansible on macOS
+  if ! command -v brew &>/dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  else
-    echo "Homebrew is already installed"
   fi
-fi
-
-# Install ansible based on the operating system
-if [ "$os" == "linux" ]; then
-  # Install ansible on Linux
-  apt-get update
-  apt-get install software-properties-common
-  apt-add-repository --yes --update ppa:ansible/ansible
-  apt-get install ansible
-elif [ "$os" == "windows" ]; then
-  # Install ansible on Windows (using Chocolatey)
-  choco install ansible
-elif [ "$os" == "MACOS" ]; then
-  # Install ansible on macOS (using Homebrew)
-  brew update
-  brew install ansible
+  if ! command -v ansible &>/dev/null; then
+    brew install ansible
+  fi
+  ansible --version
+  # Install Python on macOS
+  if ! command -v python &>/dev/null; then
+    brew install python
+  fi
+  python -V
+elif [ "$choice" = "2" ] || [ "$choice" = "Linux" ]; then
+  # Install apt and ansible on Linux
+  if ! command -v apt &>/dev/null; then
+    sudo apt-get update
+    sudo apt-get install apt
+  fi
+  if ! command -v ansible &>/dev/null; then
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository --yes --update ppa:ansible/ansible
+    sudo apt-get install ansible
+  fi
+  ansible --version
+  # Install Python on Linux
+  if ! command -v python &>/dev/null; then
+    sudo apt-get install python3
+  fi
+  python3 -V
+elif [ "$choice" = "3" ] || [ "$choice" = "Windows" ]; then
+  # Install choco and ansible on Windows
+  if ! command -v choco &>/dev/null; then
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  fi
+  if ! command -v ansible &>/dev/null; then
+    choco install ansible
+  fi
+  ansible --version
+  # Install Python on Windows
+  if ! command -v python &>/dev/null; then
+    choco install python
+  fi
+  python -V
 else
-  echo "Invalid operating system"
-  exit 1
+  echo "Invalid choice. Please enter a valid number (1, 2, or 3)."
 fi
-
-# Check the version of ansible
-ansible --version
-
-# Confirm ansible's installation
-echo "Ansible is now installed!"
-
-# Download the latest version of Python based on the operating system
-if [ "$os" == "linux" ]; then
-  # Download the latest version of Python on Linux (using pyenv)
-  apt-get update
-  apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-  libreadline-dev libsqlite3-dev wget curl llvm libncursed5-dev libncursedw5-dev \
-  xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
-  curl https://pyenv.run | bash
-  pyenv update
-  pyenv install 3.x.x
-elif [ "$os" == "windows" ]; then
-  # Download the latest version of Python on Windows (using Chocolatey)
-  choco install python
-elif [ "$os" == "MACOS" ]; then
-  # Download the latest
