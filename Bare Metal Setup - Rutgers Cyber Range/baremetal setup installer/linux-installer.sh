6f657c2424dee6e3fb85560d529cddc3d373c8c8
kwxk <76072619+kwxk@users.noreply.github.com>
2022-12-28 17:37:36 -0500

    Update linux-installer.sh

--- Bare Metal Setup - Rutgers Cyber Range/baremetal setup installer/linux-installer.sh
+++ Bare Metal Setup - Rutgers Cyber Range/baremetal setup installer/linux-installer.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-# Install yum and ansible on Linux
-if ! command -v yum &>/dev/null; then
-  sudo yum update
-  sudo yum install yum
-fi
-if ! command -v ansible &>/dev/null; then
-  sudo yum install software-properties-common
-  sudo yum-config-manager --add-repo https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/
-  sudo yum install ansible
-fi
-ansible --version
+# Update the package manager repository
+sudo yum update
 
-# Install Python on Linux
-if ! command -v python &>/dev/null; then
-  sudo yum install python3
-fi
-python3 -V 
+# Install Python
+sudo yum install python3
+
+# Install ansible
+sudo yum install ansible
